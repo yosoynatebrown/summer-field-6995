d375b46ed7539d7e575d9cb16961fdb7e1739dfc
Nathan Brown <nathan.brown263@gmail.com>
2021-11-02 11:31:22 -0600

    refactor: Tweak HTML to improve layout

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,12 +1,5 @@
 require "rails_helper"
 
-# I do not see any actors listed that are not part of the movie
-# And I see a form to add an actor to this movie
-# When I fill in the form with the name of an actor that exists in the database
-# And I click submit
-# Then I am redirected back to that movie's show page
-# And I see the actor's name is now listed
-
 RSpec.describe 'Movies show page' do
   before(:each) do
     @universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
