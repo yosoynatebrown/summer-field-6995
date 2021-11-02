d1f91bf9b4fd89ce758e7b5c411e86c710cdb350
Nathan Brown <nathan.brown263@gmail.com>
2021-11-02 11:24:22 -0600

    refactor: Add scopes to Actor model

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,13 +2,8 @@ class Actor < ApplicationRecord
   has_many :actor_movies
   has_many :movies, through: :actor_movies
 
-  def self.order_by_age
-    order(:age)
-  end
-
-  def self.average_age
-    average(:age).round(2)
-  end
+  scope :order_by_age, -> { order(:age) }
+  scope :average_age, -> { average(:age).round(2) }
 
   def coactors
     movie_ids = ActorMovie.where(actor_id: self.id).pluck(:movie_id)
